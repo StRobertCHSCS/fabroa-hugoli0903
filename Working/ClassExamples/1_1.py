2538960ff0f86d66748cf31e9cf2b4f83f4db6e6
hugoli0903 <hugo.li22@ycdsbk12.ca>
2019-10-01 09:09:20 -0400

    new live hack files

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,19 @@
+
+print("This is the text a print statement")
+print("What's wrong with this statement?")
+print("How about this one?")
+
+print("There are 3 things in a variable: a name, a type and value")
+
+Hello = "Goodbye"
+
+print(Hello,"and thank you for the service provided")
+print(3.14)
+print(5)
+
+print("Commas are used to separate values from each other")
+print("There are the ages of the three children: ",11 ,6 ,3)
+print(3.14,"is and will always be my favourite pi")
+
+colour = "purple and yellow"
+print("I've always believed that",colour, "matches very well together")
