ecff165d194518d6309c4a3b1f79200ededa3974
hugoli0903 <hugo.li22@ycdsbk12.ca>
2019-10-02 09:28:35 -0400

    Practice Question

--- Working/Practice Questions/1_2_2Operators.py
+++ Working/Practice Questions/1_2_2Operators.py
@@ -1,4 +1,4 @@
 instrument = "kazoo"
 age = str(7)
-print("I have played the " + instrument + " since I was " + age + " years old."
-)
+print("I have played the " + instrument + " since I was ", end=""
++ age + " years old.")
