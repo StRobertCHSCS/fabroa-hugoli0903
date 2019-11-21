9301bc04b159a8b6bf031565a6ab0e13beaa643d
hugoli0903 <hugo.li22@ycdsbk12.ca>
2019-11-21 09:23:19 -0500

    Practice Questions

---
+++ Working/Practice Questions/2_4_5_Logical_operators.py
@@ -0,0 +1,9 @@
+# Get number from user
+number = int(input("Enter a number: "))
+
+# Calculate the absoltue difference of the number
+if number < 21:
+    print(21-number)
+
+else: 
+    print((21-number)*2)
---
+++ Working/Practice Questions/2_4_6_Logical_operators.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+if first_number == second_number:
+    print((first_number+second_number)*2)
+
+else:
+    print(first_number+second_number)
